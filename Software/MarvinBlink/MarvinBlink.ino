32781d71d465a5dabe366ddac0b2c8e87453e5a0
ArjenvdW <arjen.van.de.wetering@nl.ibm.com>
2016-11-14 22:37:56 +0100

    Blink faster ...
    ... and print Hello World only once. Give UART time to initialize
    properly.

--- Software/MarvinBlink/MarvinBlink.ino
+++ Software/MarvinBlink/MarvinBlink.ino
@@ -10,15 +10,16 @@
 void setup() {
   // initialize digital pin 13 as an output.
   pinMode(13, OUTPUT);
-  Serial.begin(57600);
-  Serial.println("Starting program");
+  Serial.begin(74880);
+  delay(1500);
+  Serial.println("Hello World!");
 }
 
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);              // wait for a second
+  delay(500);              // wait for a second
   digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);              // wait for a second
-  Serial.println("Hello World!");
+  delay(500);              // wait for a second
+  Serial.print(".");
 }
